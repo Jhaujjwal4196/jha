89142f4be68e0c660cd99b8feadc9fb448cc4d0c
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2022-12-16 12:03:40 +0530

    added 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define lli long long int
+#define w(n) int n; cin>>n; while(n--)
+#define mod 1e9+7
+#define rep(n) for(int i=1;i<=n;i++)
+#define sc2(a,b) int a,b; cin>>a>>b;
+#define sc1(a) int a; cin>>a;
+#define sc3(a,b,c) int a,b,c; cin>>a>>b>>c;
+#define pb push_back
+#define vi vector<int>
+ 
+ 
+int main(){
+   sc1(n);
+   cout<<n<<endl;
+}
